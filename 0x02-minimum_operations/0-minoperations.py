dfe6fbd6dc02463f78a424813680aa1d84f515d8
Almujtaba Abdalla <almujtaba.abdu@gmail.com>
2024-10-18 01:00:58 +0300

    update 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Minimum Operations module"""
 from math import log2
 
 
 def minOperations(n):
-    """Calculates the minimum number of operations to achieve n 'H' characters."""
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+    """Calculates the minimum number of operations"""
+    if n <= 1:
         return 0
-    return op
+
+    operation_needed = 0
+    clipbord = 0
+    current_lenght = 1
+
+    while current_lenght < n:
+        if n % current_lenght == 0:
+            clipbord = current_lenght
+            operation_needed += 1
+
+        current_lenght += clipbord
+        operation_needed += 1
+
+    return operation_needed
